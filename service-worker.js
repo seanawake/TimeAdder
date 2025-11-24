a6b2a490013f952f3a2d0ba497df47c723ba3494
Raj Rajan <seanawake@gmail.com>
2025-11-24 11:40:25 +0530

    Implement service worker with caching and fetch handling

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,40 @@
-self.addEventListener('install', event => {
-    self.skipWaiting();
+const CACHE = 'timeadder-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css',
+  'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache => cache.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
 });
 
-self.addEventListener('fetch', event => {
-    // Basic fetch passthrough
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => (k === CACHE ? null : caches.delete(k))))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(hit => {
+      if (hit) return hit;
+      return fetch(event.request).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(() => {});
+        return res;
+      }).catch(() => caches.match('./index.html'));
+    })
+  );
 });
